b86f53f08656e12d94e4a71a0bad77a697174da4
paritoshik-98 <kharadparitoshik@gmail.com>
2022-02-13 23:54:48 +0530

    added landing page responsive

--- frontend/src/LandingPage.js
+++ frontend/src/LandingPage.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import {Link} from "react-router-dom";
+import './landing.css'
 export default function LandingPage() {
   return (
-  <>
-    <button><Link to={'/login'}>LOGIN</Link></button>
-    {/* <button><Link to={'/signup'}>SIGNUP</Link></button> */}
-  </>
+  <div className='outer'>
+     <div className='inner'>
+       <div className='login'></div>
+       <div className='register'></div>
+    {/* <button className='btn btn-light'><Link to={'/login'}>LOGIN</Link></button> 
+    <button className='btn btn-light'><Link to={'/login'}>LOGIN</Link></button> 
+    <button className='btn btn-light'><Link to={'/login'}>LOGIN</Link></button>  */}
+{/* <button><Link to={'/signup'}>SIGNUP</Link></button> */}
+    </div>
+    </div>
   )
 }
