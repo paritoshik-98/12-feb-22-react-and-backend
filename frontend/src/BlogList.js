033eb4c6d731dfb648bec021d57e8672c407a171
paritoshik-98 <kharadparitoshik@gmail.com>
2022-02-15 22:42:24 +0530

    added pagination using react-paginate

--- frontend/src/BlogList.js
+++ frontend/src/BlogList.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 import './Axios'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+
+import ReactPaginate from'react-paginate'
 
 export default function BlogList() {
 
@@ -17,12 +19,40 @@ export default function BlogList() {
         }
     },[LoginStatus.user])
 
+    const [currentPage, setCurrentPage] = useState(0);
+    const [data, setData] = useState([]);
+
     useEffect(() => {
-        axios.get('/api/blog/all').then(res=>console.log(res.data))
+        axios.get('/api/blog/all').then(res=>setData(res.data))
     })
+
+    const PER_PAGE = 1;
     
+    const offset = currentPage * PER_PAGE;
+    
+    const currentPageData = data.slice(offset, offset + PER_PAGE).map(({ title }) => <h1>{title}</h1>);
+    
+    const pageCount = Math.ceil(data.length / PER_PAGE);
+    
+    function handlePageClick({ selected: selectedPage }) {
+      setCurrentPage(selectedPage);
+  }
 
   return (
-    <div>BlogList</div>
+    <>
+    <h1>React Paginate Example</h1>
+      <ReactPaginate
+        previousLabel={"← Previous"}
+        nextLabel={"Next →"}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        previousLinkClassName={"pagination__link"}
+        nextLinkClassName={"pagination__link"}
+        disabledClassName={"pagination__link--disabled"}
+        activeClassName={"pagination__link--active"}
+      />
+      {currentPageData}
+      </>
   )
 }
--- frontend/src/LandingPage.js
+++ frontend/src/LandingPage.js
@@ -3,15 +3,25 @@ import {Link} from "react-router-dom";
 import './landing.css'
 export default function LandingPage() {
   return (
-  <div className='outer'>
-     <div className='inner'>
-       <div className='login'></div>
-       <div className='register'></div>
-    {/* <button className='btn btn-light'><Link to={'/login'}>LOGIN</Link></button> 
-    <button className='btn btn-light'><Link to={'/login'}>LOGIN</Link></button> 
-    <button className='btn btn-light'><Link to={'/login'}>LOGIN</Link></button>  */}
-{/* <button><Link to={'/signup'}>SIGNUP</Link></button> */}
+    <div className='container'>
+      <div className='row custom justify-content-center align-content-center'>
+        <div className='col-12  col-md-6'>
+          <div className='p-2 border bg-light'>col 1</div>
+        </div>
+        <div className='col-12  col-md-6'>
+          <div className='p-2 border bg-light'>col 2</div>
+        </div>
+      </div>
     </div>
-    </div>
+//   <div className='outer'>
+//      <div className='inner'>
+//        <div className='login'></div>
+//        <div className='register'></div>
+//     {/* <button className='btn btn-light'><Link to={'/login'}>LOGIN</Link></button> 
+//     <button className='btn btn-light'><Link to={'/login'}>LOGIN</Link></button> 
+//     <button className='btn btn-light'><Link to={'/login'}>LOGIN</Link></button>  */}
+// {/* <button><Link to={'/signup'}>SIGNUP</Link></button> */}
+//     </div>
+//     </div>
   )
 }
