de4fe676f6a83a82d2baaed31d37eb0d9aab9035
paritoshik-98 <kharadparitoshik@gmail.com>
2022-03-25 22:56:55 +0530

    home page states

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,7 +1,9 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getBlogListAction } from '../REDUX/Actions/blogActions'
 import './home.css'
+import '../Axios'
 
 function Home() {
 
@@ -9,9 +11,12 @@ function Home() {
 
     const selector = useSelector(state=>state.blogList)
 
+    const[trending,setTrending] = useState()
+    const[code,setCode] = useState()
   useEffect(()=>{
-    dispatch(getBlogListAction())
-    
+    // dispatch(getBlogListAction())
+    axios.get('/api/blog/trending').then(res=>setTrending(res.data)).catch(e=>console.log(e))
+    axios.get('/api/blog/code').then(res=>setCode(res.data)).catch(e=>console.log(e))
   },[])
 
   const[all,setAll]=useState(selector.blogs)
