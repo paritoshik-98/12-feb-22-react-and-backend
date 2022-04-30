a005b019ec048bd0598d833d1bd1bd9cc7fe5c33
paritoshik-98 <kharadparitoshik@gmail.com>
2022-04-30 18:47:17 +0530

    crtns

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useLayoutEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getBlogListAction } from '../REDUX/Actions/blogActions'
 import { Link } from 'react-router-dom'
@@ -20,16 +20,28 @@ function Home() {
     const[code,setCode] = useState()
     const[music,setMusic] = useState()
     
+    // const[sw,setSW]=useState(window.innerWidth)
+    // const[mv,setMV]=useState()
+    // useLayoutEffect(()=>{
+    //   if(window.innerWidth<600){
+    //     setMV(true)
+    //   }
+    //   else{
+    //     setMV(false)
+    //   }
+    // },[window.innerWidth])
+
   useEffect(()=>{
     // dispatch(getBlogListAction())
     axios.get('/api/blog/cat/all').then(res=>setTrending(res.data.posts)).catch(e=>console.log(e))
     axios.get('/api/blog/cat/code').then(res=>setCode(res.data.posts)).catch(e=>console.log(e))
     axios.get('/api/blog/cat/music').then(res=>setMusic(res.data.posts)).catch(e=>console.log(e))
+  // alert(window.innerWidth)
   },[])
 
   const[all,setAll]=useState(selector.blogs)
 
-  
+  // alert(JSON.stringify(screen.width))
 
 
   return (<>
@@ -48,9 +60,32 @@ function Home() {
       {/* style="max-width: 540px;" */}
       <h3 className=' top'>Top Article of the Day</h3>
 
-      {trending[0]?
-      <div class="card mb-3" >
+      {trending[0]?<>
+{/*  */}
+        {/* <div class="mb-3" id='EXcard' >
+  <div class="row row-cols-2 ">
+    <div class="col-8">
+      <div class="cbody">
+        <h5 class="ctitle">{trending[0].title}</h5>
+        <div className="authorInfo  d-flex">
+          <img className='auth_pic align-self-center' src={trending[0].author.profile_pic}></img>
+          <h7 className='align-self-center'>{trending[0].author.name}</h7>
+        </div>
+        <p class="ctext">{trending[0].desc}</p>
+        <Link className='rl' to ={`/read/${trending[0]._id}`} >Read More ..</Link>
+      </div>
+    </div>
+    <div class="col-4 top-a-img-ex">
+      <img src={trending[0].coverImg} class=" " alt="..."></img>
+    </div>
+  </div>
+</div> */}
+{/*  */}
+      <div class="card mb-3" id='Lcard' >
   <div class="row g-0">
+  <div class="col-md-4 top-a-img">
+      <img src={trending[0].coverImg} class="img-fluid " alt="..."></img>
+    </div>
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title">{trending[0].title}</h5>
@@ -60,22 +95,42 @@ function Home() {
         </div>
         <p class="card-text">{trending[0].desc}</p>
         <Link className='rl' to ={`/read/${trending[0]._id}`} >Read More ..</Link>
-        {/* <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> */}
       </div>
     </div>
+    
+  </div>
+</div>
+{/* <div class="card mb-3" id='Pcard' >
+  <div class="row g-0">
     <div class="col-md-4 top-a-img">
       <img src={trending[0].coverImg} class="img-fluid " alt="..."></img>
     </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">{trending[0].title}</h5>
+        <div className="authorInfo  d-flex">
+          <img className='auth_pic align-self-center' src={trending[0].author.profile_pic}></img>
+          <h7 className='align-self-center'>{trending[0].author.name}</h7>
+        </div>
+        <p class="card-text">{trending[0].desc}</p>
+        <Link className='rl' to ={`/read/${trending[0]._id}`} >Read More ..</Link>
+      </div>
+    </div>
   </div>
-</div>
+</div> */}
+</>
 :null}
-
+{/* mobile  screen */}
+{/* {
+  ()?{}
+} */}
+{/*  */}
 <div className=" cat d-flex justify-content-between align-content-center mt-5">
 <h3>Most Popular </h3>
 <Link to='/cat/all' className='link' href=''>View More --></Link>
 </div>
 <div className="trending row row-cols-1 row-cols-sm-2 g-4">
-<div class="col " style={{width:'40%'}}>
+<div class="col " id='p4'>
 {trending[1]?
     <div class="card h-100" >
       <img src={trending[1].coverImg} class="card-img-top homeImage" alt="..."></img>
@@ -91,7 +146,7 @@ function Home() {
     </div>
     :null}
   </div>
-<div class="col " style={{width:'60%'}}>
+<div class="col " id='p6'>
 {trending[2]?
     <div class="card h-100" >
     <img src={trending[2].coverImg} class="card-img-top homeImage" alt="..."></img>
@@ -107,7 +162,7 @@ function Home() {
   </div>
     :null}
   </div>
-<div class="col" style={{width:'60%'}}>
+<div class="col" id='p6e' >
 {trending[3]?
     <div class="card h-100" >
     <img src={trending[3].coverImg} class="card-img-top homeImage" alt="..."></img>
@@ -123,7 +178,7 @@ function Home() {
   </div>
     :null}
   </div>
-<div class="col" style={{width:'40%'}}>
+<div class="col" id='p4e'>
 {trending[4]?
     <div class="card h-100" >
     <img src={trending[4].coverImg} class="card-img-top homeImage" alt="..."></img>
@@ -145,7 +200,7 @@ function Home() {
 <Link to='/cat/code' className='link'>View More --></Link>
 </div>
 <div className="trending row row-cols-1 row-cols-sm-2 g-4">
-<div class="col " style={{width:'40%'}}>
+<div class="col " id='p4'>
 {code[0]?
     <div class="card h-100" >
     <img src={code[0].coverImg} class="card-img-top homeImage" alt="..."></img>
@@ -161,7 +216,7 @@ function Home() {
   </div>
     :null}
   </div>
-<div class="col" style={{width:'60%'}}>
+<div class="col" id='p6' >
 {code[1]?
     <div class="card h-100" >
     <img src={code[1].coverImg} class="card-img-top homeImage" alt="..."></img>
@@ -177,7 +232,7 @@ function Home() {
   </div>
     :null}
   </div>
-<div class="col" style={{width:'60%'}}>
+<div class="col" id='p6e'>
 {code[2]?
     <div class="card h-100" >
     <img src={code[2].coverImg} class="card-img-top homeImage" alt="..."></img>
@@ -193,7 +248,7 @@ function Home() {
   </div>
     :null}
   </div>
-<div class="col" style={{width:'40%'}}>
+<div class="col" id='p4e'>
 {code[3]?
     <div class="card h-100" >
     <img src={code[3].coverImg} class="card-img-top homeImage" alt="..."></img>
@@ -215,7 +270,7 @@ function Home() {
 <Link to='/cat/music' className='link'>View More --></Link>
 </div>
 <div className="trending row row-cols-1 row-cols-sm-2 g-4">
-<div class="col " style={{width:'40%'}}>
+<div class="col " id='p4'>
 {music[0]?
     <div class="card h-100" >
     <img src={music[0].coverImg} class="card-img-top homeImage" alt="..."></img>
@@ -231,7 +286,7 @@ function Home() {
   </div>
     :null}
   </div>
-<div class="col" style={{width:'60%'}}>
+<div class="col" id='p6'>
 {music[1]?
     <div class="card h-100" >
     <img src={music[1].coverImg} class="card-img-top homeImage" alt="..."></img>
@@ -247,7 +302,7 @@ function Home() {
   </div>
     :null}
   </div>
-<div class="col" style={{width:'60%'}}>
+<div class="col" id='p6e'>
 {music[2]?
     <div class="card h-100" >
     <img src={music[2].coverImg} class="card-img-top homeImage" alt="..."></img>
@@ -263,7 +318,7 @@ function Home() {
   </div>
     :null}
   </div>
-<div class="col" style={{width:'40%'}}>
+<div class="col" id='p4e'>
 {music[3]?
     <div class="card h-100" >
     <img src={music[3].coverImg} class="card-img-top homeImage" alt="..."></img>
